74373883990144f224f895449210c51f0eab8adf
lenayoo <lenayoo17@gmail.com>
2024-06-22 13:08:55 +0900

    fix structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello Lena!</h1>
+      <div className="pb-2">
+        <h1 className="text-3xl font-bold underline">Hello Lena!</h1>
+      </div>
+      <AddCircleIcon fontSize="large" color="action" />
     </>
   );
 }
