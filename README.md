9c1097ad1c9b63649980c40ddb4a77c0ca269d4e
lenayoo <lenayoo17@gmail.com>
2024-06-23 00:06:47 +0900

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # React + TypeScript + Vite
 
 preDesign for first swift app
+
+- [ ] make 100days app
+- [ ] add countdown function
+- [ ] login function
+- [ ] add responsive
+- [ ] deploy in firebase
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,5 +1,8 @@
 const Countdown = () => {
-  return <div>Countdown</div>;
+  const today = new Date();
+  const hundredDays = new Date(today.getTime() + 100 * 24 * 60 * 60 * 1000);
+
+  return <div>{hundredDays.toDateString()}</div>;
 };
 
 export default Countdown;
